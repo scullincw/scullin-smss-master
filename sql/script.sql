228d3e65c92f92ca0c286c46bbb66dc009d115a1
Scullincw <1148135455@qq.com>
2019-12-19 22:26:25 +0800

    重新添加了Java代码, 整个web应用的功能完全可用

--- sql/script.sql
+++ sql/script.sql
@@ -1,4 +1,15 @@
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 
+CREATE DATABASE smss;
+
 use smss;
 select * FROM smss.system;
+
+SHOW TABLES;
+
+SELECT * FROM teacher;
+
+DELETE FROM teacher
+WHERE id = 1;
+
+SELECT * FROM smss.system;
